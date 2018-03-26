c4b467c0e469c2c9a761421975604dcf2550e26b
eyi1 <ej7729@gmail.com>
2018-03-26 18:19:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 def fizzbuzz (num)
   if num % 3 == 0
-    return "Fizz"
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  else
+    "nil"
   end
 end
